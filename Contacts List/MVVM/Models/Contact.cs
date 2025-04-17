b66b7eea5762acb5a854b6ba2d08dc766ea473db
Davi <Davi@DESKTOP-LI13EEL>
2025-04-17 19:36:09 -0300

    Update + Added nuget packages and created Contacts.cs model class

---
+++ Contacts List/MVVM/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+
+namespace Contacts_List.MVVM.Models
+{
+    [Table("Contacts")]
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull, MaxLength(100), Unique]
+        public string Name { get; set; }
+
+        [NotNull, MaxLength(30), Unique]
+        public string PhoneNumber { get; set; }
+
+        [MaxLength(100)]
+        public string? Email { get; set; }
+
+        [MaxLength(150)]
+        public string? Address { get; set; }
+
+        public byte[]? ProfileImage { get; set; }
+
+        [MaxLength(500)] 
+        public string? Notes { get; set; }
+    }
+}
